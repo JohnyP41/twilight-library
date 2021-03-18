281ead81f23b06f46c933911194da2e1e40b0375
Jan Przybylski <plaljahn@gmail.com>
2021-03-18 21:28:56 +0100

    update

--- app/robot.hs
+++ app/robot.hs
@@ -6,36 +6,27 @@ import Data.List.Utils (replace)
 import Text.Regex.Posix
 import Text.Printf
 
-
-extractRecords = extractLinksWithText "//a[contains(@href,'.pdf')]"  -- pary adres-tytuł
+extractRecords = extractLinksWithText "//a[contains(@href,'.htm')]" 
                  >>> second (arr $ replace "\r\n            " " ") 
-                 >>> first (arr ((++"tr") . init)) 
-                 >>> first (extractLinksWithText "//li/a[contains(@href,'.pdf')]")
-                 
+                 >>> first (extractLinksWithText "//a[contains(@href,'.pdf')]")
                  
                  
 toShadowItem :: ((String, String), String) -> ShadowItem
 toShadowItem ((url, articleTitle), yearlyTitle) =
   (defaultShadowItem url title) {
-    originalDate = Just date,
+    originalDate = Nothing,
     itype = "periodical",
     format = Just "pdf",
     finalUrl = url
     }
+
   where title = "Pte " ++ yearlyTitle ++ " " ++ (replace "\r\n" "" (replace "\r\n          " "" articleTitle))
-        date = getDate url
-
-getDate url =
-  case url =~~ "/(19[0-9][0-9]|20[0-9][0-9])/" :: Maybe [[String]] of
-    Just [[_, year]] -> year
-    otherwise -> error $ "unexpected url: " ++ url
-
 
 main = do
-    let start = "http://pte.au.poznan.pl"
+    let start = "http://pte.au.poznan.pl/PTE_left.htm"
     let shadowLibrary = ShadowLibrary {logoUrl=Nothing,
-                                       lname="Pte.au.poznan",
-                                       abbrev="Pte",
-                                       lLevel=0,
-                                       webpage=start}
+                                        lname="Pte.au.poznan",
+                                        abbrev="Pte",
+                                        lLevel=0,
+                                        webpage="http://pte.au.poznan.pl/"}
     extractItemsStartingFromUrl shadowLibrary start (extractRecords >>> arr toShadowItem)
